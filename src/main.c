1ba15126712150c4ecc2d9d21d12e9a393b15d20
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-21 00:50:37 +0100

    Add test code for creating, initalizing and deleting a Chip8 instance

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "chip8.h"
 
 int main(void)
 {
+    Chip8 chip = chip8_create();
+    if (chip == NULL) {
+        fprintf(stderr, "Chip-8 Error: Could not allocate memory");
+        exit(EXIT_FAILURE);
+    }
+    chip8_initialize(chip);
 
-    return 0;
+    chip8_delete(chip);
+
+    exit(EXIT_SUCCESS);
 }
