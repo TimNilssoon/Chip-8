5eef782ff63d7b90e0a581e917304652c2c9a517
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-21 00:46:47 +0100

    Add chip8.h

---
+++ src/chip8.h
@@ -0,0 +1,13 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+#include <stdlib.h>
+
+// Chip8 is an ADT of a chip8_t struct pointer to 
+typedef struct chip8_t *Chip8;
+
+Chip8 chip8_create(void);
+void chip8_delete(Chip8 c);
+void chip8_initialize(Chip8 c);
+//void chip8_load_rom(Chip8 c, const char *path);
+
+#endif
