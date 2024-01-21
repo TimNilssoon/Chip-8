b4d3c91bc318425c6ef455f59c554d60be4f08df
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-21 02:03:45 +0100

    Define DISPLAY_WIDTH and DISPLAY_HEIGHT in chip8.h

--- src/chip8.h
+++ src/chip8.h
@@ -1,6 +1,10 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 #include <stdlib.h>
+#include <stdint.h>
+
+#define DISPLAY_WIDTH 64
+#define DISPLAY_HEIGHT 32
 
 // Chip8 is an ADT of a chip8_t struct pointer
 typedef struct chip8_t *Chip8;
@@ -9,5 +13,8 @@ Chip8 chip8_create(void);
 void chip8_destroy(Chip8 c);
 void chip8_initialize(Chip8 c);
 //void chip8_load_rom(Chip8 c, const char *path);
+
+// Returns the display buffer of the Chip-8 (uint16_t * 2048)
+const uint16_t *chip8_get_display_buffer(const Chip8 c);
 
 #endif
