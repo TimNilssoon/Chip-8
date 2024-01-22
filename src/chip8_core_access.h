2d0db61f47215176e5c85cdbad0c32d5086b4fa3
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-22 01:33:28 +0100

    Add a few functions to opcodeHandler interface

--- src/chip8_core_access.h
+++ src/chip8_core_access.h
@@ -1,11 +1,17 @@
-#ifndef CHIP8_CORE_INTERFACE_H
-#define CHIP8_CORE_INTERFACE_H
+#ifndef CHIP8_CORE_OPCODEFUNC_H
+#define CHIP8_CORE_OPCODEFUNC_H
 
 #include <stdint.h>
 
 // Chip8Core is an ADT of a chip8_core_t struct pointer
 typedef struct chip8_core_t *Chip8Core;
 
+const uint16_t *chip8_core_getDisplayBuffer(const Chip8Core c);
+
 const uint16_t *chip8_core_getOpcode(const Chip8Core c);
+
+void chip8_core_clearDisplayBuffer(Chip8Core c);
+void chip8_core_returnFromSubroutine(Chip8Core c);
+void chip8_core_jumpToAddress(Chip8Core c);
 
 #endif
