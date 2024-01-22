23e5a60ff78ffe023b28ac1cdc9bde7fd28e4337
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-22 23:47:37 +0100

    Rename

---
+++ src/chip8_core_instructions.h
@@ -0,0 +1,35 @@
+#ifndef CHIP8_CORE_OPCODEFUNC_H
+#define CHIP8_CORE_OPCODEFUNC_H
+
+#include <stdint.h>
+
+typedef struct chip8_core_t *Chip8Core;
+
+const uint16_t *chip8_core_getOpcode(const Chip8Core c);
+
+void chip8_core_CLS(Chip8Core c);
+void chip8_core_RET(Chip8Core c);
+void chip8_core_JPNNN(Chip8Core c);
+void chip8_core_CALLNNN(Chip8Core c);
+void chip8_core_SEVXKK(Chip8Core c);
+void chip8_core_SNEVXKK(Chip8Core c);
+void chip8_core_SEVXVY(Chip8Core c);
+void chip8_core_LDVXKK(Chip8Core c);
+void chip8_core_ADDVXKK(Chip8Core c);
+void chip8_core_LDINNN(Chip8Core c);
+void chip8_core_SNEVXVY(Chip8Core c);
+void chip8_core_JPV0NNN(Chip8Core c);
+void chip8_core_RNDVXKK(Chip8Core c);
+void chip8_core_SKPVX(Chip8Core c);
+void chip8_core_SKNPVX(Chip8Core c);
+void chip8_core_LDVXDT(Chip8Core c);
+void chip8_core_LDVXK(Chip8Core c);
+void chip8_core_LDDTVX(Chip8Core c);
+void chip8_core_LDSTVX(Chip8Core c);
+void chip8_core_ADDIVX(Chip8Core c);
+void chip8_core_LDFVX(Chip8Core c);
+void chip8_core_LDBVX(Chip8Core c);
+void chip8_core_LDIVX(Chip8Core c);
+void chip8_core_LDVXI(Chip8Core c);
+
+#endif
