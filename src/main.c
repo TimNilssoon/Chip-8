495f143c55eb12b74bf2fe3206c07f5f64b3764b
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-29 02:27:15 +0100

    Add null pointer check

--- src/main.c
+++ src/main.c
@@ -9,11 +9,15 @@ int main(int argc, char *argv[])
         return 1;
     }
     
-    Chip8 chip8;
-    chip8_initialize(&chip8);
-    chip8_loadRom(&chip8, argv[1]);
-    chip8_run(&chip8);
+    Chip8 *chip8 = chip8_create();
+    if (chip8 == NULL) {
+        fprintf(stderr, "Chip8 error: Could not allocate memory for Chip-8");
+        exit(EXIT_FAILURE);
+    }
+    chip8_initialize(chip8);
+    chip8_loadRom(chip8, argv[1]);
+    chip8_run(chip8);
 
-    chip8_destroy(&chip8);
+    chip8_destroy(chip8);
     return 0;
 }
